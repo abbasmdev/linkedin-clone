9471449ba2072a624c8fb9e9e9987d9ac874bb80
Abbas Moharami <abbasmdev@gmail.com>
2021-06-21 22:00:09 +0430

    added PostItem cmp for showing posts in Feed cmp.

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,9 +1,31 @@
 import PostForm from "./PostForm";
 import styles from "./index.module.css";
+import PostItem from "./PostItem";
+import { useState } from "react";
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
   return (
     <div className={styles.feed}>
-      <PostForm />
+      <PostForm
+        name="Abbas Moharami"
+        description="Test desc"
+        message="Test message"
+      />
+      <PostItem
+        name="Abbas Moharami"
+        description="Test desc"
+        message="Test message"
+      />
+      <PostItem
+        name="Abbas Moharami"
+        description="Test desc"
+        message="Test messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest messageTest message"
+      />
+      <PostItem
+        name="Abbas Moharami"
+        description="Test desc"
+        message="Test message"
+      />
     </div>
   );
 };
